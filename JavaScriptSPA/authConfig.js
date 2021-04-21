53b4169845082c9d45f59c6a3f4c148917a49d71
luizrosalba <luizrosalba@gmail.com>
2021-04-21 20:48:44 -0300

    first commit

---
+++ JavaScriptSPA/authConfig.js
@@ -0,0 +1,26 @@
+ 
+// Config object to be passed to Msal on creation.
+// For a full list of msal.js configuration parameters, 
+// visit https://azuread.github.io/microsoft-authentication-library-for-js/docs/msal/modules/_authenticationparameters_.html
+const msalConfig = {
+  auth: {
+    clientId: "042b7461-8d15-4d69-98b2-74df1a0030df",
+    authority: "https://login.microsoftonline.com/da49a844-e2e3-40af-86a6-c3819d704f49",
+    redirectUri: "http://localhost:3000/",
+  },
+  cache: {
+    cacheLocation: "sessionStorage", // This configures where your cache will be stored
+    storeAuthStateInCookie: true, // Set this to "true" if you are having issues on IE11 or Edge
+  }
+};  
+  
+// Add here the scopes to request when obtaining an access token for MS Graph API
+// for more, visit https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-core/docs/scopes.md
+const loginRequest = {
+  scopes: ["openid", "profile", "User.Read"]
+};
+
+// Add here scopes for access token to be used at MS Graph API endpoints.
+const tokenRequest = {
+  scopes: ["Mail.Read"]
+};
