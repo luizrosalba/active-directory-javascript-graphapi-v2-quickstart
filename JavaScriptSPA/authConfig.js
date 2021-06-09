2b2b8862e07a4781a99420b6cc16d5388c381be6
Luiz Rosalba <luizrosalba@gmail.com>
2021-06-09 07:49:34 -0300

    Update authConfig.js

--- JavaScriptSPA/authConfig.js
+++ JavaScriptSPA/authConfig.js
@@ -5,7 +5,7 @@
 const msalConfig = {
   auth: {
     clientId: "eeb1f3c5-ec03-44aa-8639-0c977d2ad5c2",
-    authority: "https://login.microsoftonline.com/5da575e2-6108-416c-b3f8-b68bb440b7b9", ///sem distinçao app ou ent 
+    authority: "https://login.microsoftonline.com/common/", ///sem distinçao app ou ent 
     redirectUri: "https://gallant-leavitt-cb78df.netlify.app/"
   },
   cache: {
